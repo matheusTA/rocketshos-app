a1ee698a4fe316492ae1bff82ade850b9520fe9b
MatheusTA <matrajano12@gmail.com>
2019-12-14 01:00:08 -0300

    Criando as rotas do app

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,43 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 
-const Routes = createAppContainer(createBottomTabNavigator({
-  Home,
-  Cart,
-}));
+const Routes = createAppContainer(
+  createBottomTabNavigator({
+    Home: {
+      screen: Home,
+      navigationOptions: () => ({
+        tabBarIcon: ({ tintColor, focused }) => {
+          const iconName = `home${focused ? '' : '-outline'}`;
+          return (<Icon name={iconName} color={tintColor} size={30} />);
+        },
+      }),
+    },
+    Cart: {
+      screen: Cart,
+      navigationOptions: () => ({
+        tabBarIcon: ({ tintColor, focused }) => {
+          const iconName = `cart${focused ? '' : '-outline'}`;
+          return (<Icon name={iconName} color={tintColor} size={30} />);
+        },
+      }),
+    },
+  }, {
+    tabBarOptions: {
+      activeTintColor: '#F8F8F8',
+      inactiveTintColor: '#F8F8F8',
+      style: {
+        backgroundColor: '#7159c1',
+        borderTopColor: '#7159c1',
+      },
+    },
+  }),
+);
 
 export default Routes;
