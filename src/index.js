77dfdf60c67e918f0d023a23483382a37f385efd
MatheusTA <matrajano12@gmail.com>
2019-12-14 00:59:51 -0300

    Adicionando background

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
-
 import '~/config/ReactotronConfig';
-
+import 'react-native-gesture-handler';
+import { ImageBackground, StyleSheet, StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './store';
-
 import Routes from '~/routes';
-import 'react-native-gesture-handler';
+import background from './assets/images/background.png';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 const App = () => (
-  <Provider store={store}>
-    <Routes />
-  </Provider>
+  <ImageBackground
+    source={background}
+    resizeMode="cover"
+    style={styles.container}
+  >
+    <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  </ImageBackground>
 );
 
 export default App;
