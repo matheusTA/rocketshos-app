e187ee674958134a5aa8e4f7561a2b4de405656e
MatheusTA <matrajano12@gmail.com>
2019-12-20 18:16:53 -0300

    Refatorando chamada a api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,14 +9,12 @@ export default function Home() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    async function loadPorducts() {
-      const response = await api.get('/products');
-
-      setProducts(response.data);
-      setLoading(false);
-    }
-
-    loadPorducts();
+    api.get('/products')
+      .then((response) => {
+        setProducts(response.data);
+        setLoading(false);
+      })
+      .catch();
   }, []);
 
   return (
