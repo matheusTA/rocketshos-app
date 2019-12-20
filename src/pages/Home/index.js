def87c1ebe7457320111b38a3a42c2111261b61a
MatheusTA <matrajano12@gmail.com>
2019-12-20 05:35:34 -0300

    Implementando page Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text } from 'react-native';
-
-// import { Container } from './styles';
+import api from '../../services/api';
+import LoadingShoes from '../../components/LoadingShoes';
+import { Container } from './styles';
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadPorducts() {
+      const response = await api.get('/products');
+
+      setProducts(response.data);
+      setLoading(false);
+    }
+
+    loadPorducts();
+  }, []);
+
   return (
-    <Text>Home</Text>
+    <Container>
+      {loading ? (<LoadingShoes loading={loading} />)
+        : (<Text>Olaasdfasd asd</Text>)}
+    </Container>
   );
 }
