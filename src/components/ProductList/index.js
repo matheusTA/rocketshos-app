419c89e3b2a4f0fd4fc57b4f87cff515323673e9
MatheusTA <matrajano12@gmail.com>
2019-12-28 01:47:46 -0300

    Criando estrutura do component Product

---
+++ src/components/ProductList/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+import Protypes from 'prop-types';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {
+  Product,
+  ProductImage,
+  ProductTitle,
+  ProductPrice,
+  ProductAddButton,
+  AddButtonText,
+  ProductAmount,
+  ProductAmountText,
+} from './styles';
+
+export default function ProductList({ products }) {
+  return (
+    <FlatList
+      data={products}
+      keyExtractor={(product) => product.id}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      renderItem={({ item }) => (
+        <Product>
+          <ProductImage
+            source={{ uri: item.image }}
+          />
+
+          <ProductTitle>{item.title}</ProductTitle>
+          <ProductPrice>{item.priceFormat}</ProductPrice>
+          <ProductAddButton>
+            <ProductAmount>
+              <Icon name="add-shopping-cart" color="#FFF" size={20} />
+              <ProductAmountText>0</ProductAmountText>
+            </ProductAmount>
+            <AddButtonText>ADICIONAR</AddButtonText>
+          </ProductAddButton>
+        </Product>
+      )}
+    />
+  );
+}
+
+ProductList.propTypes = {
+  products: Protypes.arrayOf(Protypes.shape({
+    id: Protypes.number,
+    title: Protypes.string,
+    image: Protypes.string,
+    price: Protypes.number,
+    priceFormat: Protypes.string,
+  })).isRequired,
+};
