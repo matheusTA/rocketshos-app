4bf8b2896f03c88e17284b2d8c2a89e775057cc7
MatheusTA <matrajano12@gmail.com>
2019-12-29 01:58:38 -0300

    Criando o saga do cart

---
+++ src/store/sagas/cart.js
@@ -0,0 +1,50 @@
+import { Alert } from 'react-native';
+import { call, put, select } from 'redux-saga/effects';
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
+import { Creators as CartActions } from '../ducks/cart';
+
+export function* addToCart({ id }) {
+  const productExists = yield select(
+    (state) => state.cart.products.find((p) => p.id === id),
+  );
+
+  const stock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    Alert.alert('Quantidade solicitada fora de estoque!');
+    return;
+  }
+
+  if (productExists) {
+    yield put(CartActions.updateAmountSuccess(id, amount));
+  } else {
+    const response = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+
+    yield put(CartActions.addCartSuccess(data));
+  }
+}
+
+export function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
+
+  const stock = yield call(api.get, `/stock/${id}`);
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    Alert.alert('Quantidade solicitada fora de estoque!');
+    return;
+  }
+
+  yield put(CartActions.updateAmountSuccess(id, amount));
+}
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,5 +1,10 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest } from 'redux-saga/effects';
+import { addToCart, updateAmount } from './cart';
+import { Types } from '../ducks/cart';
 
 export default function* rootSaga() {
-  yield all([]);
+  yield all([
+    takeLatest(Types.ADD_CART_REQUEST, addToCart),
+    takeLatest(Types.UPDATE_AMOUNT_REQUEST, updateAmount),
+  ]);
 }
