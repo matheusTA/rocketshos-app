62c419d482b8bbb1f27bbb220398555345ece860
MatheusTA <matrajano12@gmail.com>
2020-01-02 19:08:32 -0300

    Adicionando componente para carrinho vazio

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Text } from 'react-native';
 
-// import { Container } from './styles';
+import { Container, ContainerCartOff, TextCartOff } from './styles';
 
 export default function Cart() {
+  const products = useSelector((state) => state.cart.products);
+
   return (
-    <Text>Cart</Text>
+    <Container>
+      {products.length > 0
+        ? <Text>Itens</Text>
+        : (
+          <ContainerCartOff>
+            <Icon name="cart-off" color="#FFF" size={100} />
+            <TextCartOff>Sem itens no carrinho</TextCartOff>
+          </ContainerCartOff>
+        )}
+    </Container>
   );
 }
---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  margin: auto;
+`;
+
+export const ContainerCartOff = styled.View`
+  align-items: center
+`;
+
+export const TextCartOff = styled.Text`
+  font-size: 25px;
+  color: #fff;
+  margin: 15px 0 0 0;
+`;
