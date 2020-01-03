3eed522e960cf73e785fdb1442c0b93436ec9292
MatheusTA <matrajano12@gmail.com>
2020-01-03 02:35:00 -0300

    Finalizando paginda e funcionalidade da tela de carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,20 +1,113 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { Text } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import loadingAnimeted from '../../../loadingAnimeted.json';
+import { Creators as CartActions } from '../../store/ducks/cart';
+import { formatPrice } from '../../util/format';
 
-import { Container, ContainerCartOff, TextCartOff } from './styles';
+import {
+  Container,
+  Product,
+  ProductInfo,
+  ProductImage,
+  ProductDetails,
+  ProductTitle,
+  ProductPrice,
+  ProductDelete,
+  ProductControls,
+  ProductControlButton,
+  ProductAmount,
+  ProductSubtotal,
+  ContainerCartOff,
+  TextCartOff,
+  TotalContainer,
+  TotalText,
+  TotalPrice,
+  Order,
+  OrderText,
+  LoadingIcon,
+} from './styles';
 
 export default function Cart() {
-  const products = useSelector((state) => state.cart.products);
+  const dispatch = useDispatch();
+  const loadingAddToCart = useSelector((state) => state.cart.loading);
+  const totalPrice = useSelector((state) => formatPrice(
+    state.cart.products.reduce((totalSum, product) => totalSum + product.amount * product.price, 0),
+  ));
+  const products = useSelector((state) => state.cart.products.map((product) => ({
+    ...product,
+    subtotal: formatPrice(product.amount * product.price),
+  })));
+
+  function incrementAmount(product) {
+    dispatch(CartActions.updateAmountRequest(product.id, product.amount + 1));
+  }
+
+  function decrementAmount(product) {
+    dispatch(CartActions.updateAmountRequest(product.id, product.amount - 1));
+  }
 
   return (
     <Container>
-      {products.length > 0
-        ? <Text>Itens</Text>
+      {products.length
+        ? (
+          <>
+            {products.map((product) => (
+              <Product key={product.id}>
+                <ProductInfo>
+                  <ProductImage source={{ uri: product.image }} />
+                  <ProductDetails>
+                    <ProductTitle>{product.title}</ProductTitle>
+                    <ProductPrice>{formatPrice(product.price)}</ProductPrice>
+                  </ProductDetails>
+                  <ProductDelete onPress={() => dispatch(CartActions.removeCart(product.id))}>
+                    <Icon
+                      name="delete-forever"
+                      size={24}
+                      color="#7159c1"
+                    />
+                  </ProductDelete>
+                </ProductInfo>
+                <ProductControls>
+                  <ProductControlButton onPress={() => decrementAmount(product)}>
+                    <Icon
+                      name="remove-circle-outline"
+                      size={20}
+                      color="#7159c1"
+                    />
+                  </ProductControlButton>
+                  <ProductAmount value={String(product.amount)} />
+                  <ProductControlButton onPress={() => incrementAmount(product)}>
+                    <Icon
+                      name="add-circle-outline"
+                      size={20}
+                      color="#7159c1"
+                    />
+                  </ProductControlButton>
+                  <ProductSubtotal>{product.subtotal}</ProductSubtotal>
+                </ProductControls>
+              </Product>
+            ))}
+            <TotalContainer>
+              <TotalText>Total</TotalText>
+              <TotalPrice>{totalPrice}</TotalPrice>
+              <Order>
+                {loadingAddToCart
+                  ? (
+                    <LoadingIcon
+                      source={loadingAnimeted}
+                      autoPlay={loadingAddToCart}
+                      loop={loadingAddToCart}
+                    />
+                  )
+                  : (<OrderText>FINALIZAR PEDIDO</OrderText>)}
+              </Order>
+            </TotalContainer>
+          </>
+        )
         : (
           <ContainerCartOff>
-            <Icon name="cart-off" color="#FFF" size={100} />
+            <Icon name="remove-shopping-cart" color="#999" size={100} />
             <TextCartOff>Sem itens no carrinho</TextCartOff>
           </ContainerCartOff>
         )}
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -1,7 +1,68 @@
 import styled from 'styled-components/native';
+import Lottie from 'lottie-react-native';
 
 export const Container = styled.View`
-  margin: auto;
+  margin: 15px;
+  padding: 10px;
+  background: #fff;
+  border-radius: 4px;
+`;
+
+export const Product = styled.View`
+`;
+
+export const ProductInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ProductImage = styled.Image`
+ height: 80px;
+  width: 80px;
+`;
+
+export const ProductDetails = styled.View`
+  flex: 1;
+  margin-left: 10px;
+`;
+
+export const ProductTitle = styled.Text`
+  font-size: 17px;
+`;
+
+export const ProductPrice = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+export const ProductDelete = styled.TouchableOpacity`
+`;
+
+export const ProductControls = styled.View`
+ flex-direction: row;
+  align-items: center;
+  background: #eee;
+  padding: 8px;
+  border-radius: 4px;
+`;
+export const ProductControlButton = styled.TouchableOpacity`
+`;
+export const ProductAmount = styled.TextInput.attrs({
+  readonly: true,
+})`
+  background: #fff;
+  padding: 5px;
+  margin: 0 5px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  min-width: 52px;
+`;
+
+export const ProductSubtotal = styled.Text`
+  font-weight: bold;
+  font-size: 16px;
+  flex: 1;
+  text-align: right;
 `;
 
 export const ContainerCartOff = styled.View`
@@ -10,6 +71,43 @@ export const ContainerCartOff = styled.View`
 
 export const TextCartOff = styled.Text`
   font-size: 25px;
-  color: #fff;
+  color: #999;
   margin: 15px 0 0 0;
+`;
+
+export const TotalContainer = styled.View`
+  margin-top: 30px;
+`;
+
+export const TotalText = styled.Text`
+  text-align: center;
+  font-size: 17px;
+  color: #999;
+  font-weight: bold;
+`;
+
+export const TotalPrice = styled.Text`
+  text-align: center;
+  font-size: 24px;
+  font-weight: bold;
+  margin-top: 10px;
+  margin-bottom: 15px;
+`;
+
+export const Order = styled.TouchableOpacity`
+  padding: 12px;
+  border-radius: 4px;
+  background: #7159c1;
+  align-items: center;
+`;
+
+export const OrderText = styled.Text`
+  text-align: center;
+  color: #fff;
+  font-size: 17px;
+`;
+
+export const LoadingIcon = styled(Lottie)`
+  width: 20px;
+  height: 20px;
 `;
